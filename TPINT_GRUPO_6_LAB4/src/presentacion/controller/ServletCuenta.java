6ad3a1267b4aaed7fe3c840fdd5b319f71e825fb
roruiz93 <69327406+roruiz93@users.noreply.github.com>
2024-07-06 20:05:54 -0300

    optimize codigo del servlet cuentas

--- TPINT_GRUPO_6_LAB4/src/presentacion/controller/ServletCuenta.java
+++ TPINT_GRUPO_6_LAB4/src/presentacion/controller/ServletCuenta.java
@@ -26,6 +26,8 @@ public class ServletCuenta extends HttpServlet {
 
 	private CuentaNegocio cuentaNeg = new CuentaNegocioImpl();		
 	private ClienteNegocio clienteNeg = new ClienteNegImpl();
+	private String mensaje = null;
+	private String claseMensaje = null;
 
 	public ServletCuenta() {
 		super();
@@ -79,145 +81,22 @@ public class ServletCuenta extends HttpServlet {
 			throws ServletException, IOException {
 
 		String opcion = request.getParameter("opcion");
-		String mensaje = null;
-		String claseMensaje = null;
 
 
 		switch (opcion) {
 		case "agregarCuenta":
-			Cuenta cuentagregada = new Cuenta();
-			
-			try {
-
-				Cliente cliente = new Cliente();
-				cliente.setIdCliente(Integer.parseInt(request.getParameter("clienteId")));
-
-				TipoCuenta tipodecuenta = new TipoCuenta();
-				tipodecuenta.setId(Integer.parseInt(request.getParameter("ddlTipoCuenta")));
-				
-				
-
-				cuentagregada.setCliente(cliente);
-				cuentagregada.setFechaCreacion(LocalDateTime.now());
-				cuentagregada.setTipoCuenta(tipodecuenta);
-				cuentagregada.setCbu(request.getParameter("txtCbu"));
-				cuentagregada.setSaldo(new BigDecimal(request.getParameter("txtSaldo")));
-				cuentagregada.setEstado(true);
-
-				boolean cuentaCreada = cuentaNeg.insert(cuentagregada);
-
-				if (cuentaCreada) {
-					mensaje = "La cuenta fue creada con éxito";
-					claseMensaje = "alert alert-success";
-				} else {
-					mensaje = "La cuenta no pudo ser creada ";
-					claseMensaje = "alert alert-danger";
-				}
-
-			} catch (NullPointerException e) {
-				mensaje = "La cuenta no pudo ser creada " + e.getMessage();
-				claseMensaje = "alert alert-danger";
-				e.printStackTrace();
-
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			} 
-			request.setAttribute("txtMensajeCuenta", mensaje);
-			request.setAttribute("claseMensajeCuenta", claseMensaje);
-			request.getRequestDispatcher("/Cuentas.jsp").forward(request, response);
-
+			eventoAgregarCuenta( request, response);
 			break;
 
 		case "buscarCliente":
 
-			String dni = request.getParameter("dniCliente");
-
-			try {
-				Cliente clienteServlet = clienteNeg.buscarClienteXDNI(dni);
-
-				if (clienteServlet != null) {
-
-					List<Cuenta> cuentas = cuentaNeg.cuentasXCliente(clienteServlet.getIdCliente());
-
-					request.setAttribute("clienteServlet", clienteServlet);
-					request.setAttribute("cuentasxCliente", cuentas);
-					
-				} else {
-
-					request.setAttribute("errorBusqueda", true);
-				}
-
-				request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
-
-			} catch (NullPointerException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			}
+			eventoBuscarCliente( request,response);
+			
 			break;
 
 		case "cambiarEstado":
 
-			int idCuenta = Integer.parseInt(request.getParameter("idCuenta"));
-			boolean estado = !Boolean.parseBoolean(request.getParameter("estado"));
-			String dni2 = request.getParameter("dniCliente");
-			Cliente clienteServlet = clienteNeg.buscarClienteXDNI(dni2);
-			
-			try {
-				
-				List<Cuenta> cuentasActivas = cuentaNeg.cuentasXCliente(clienteServlet.getIdCliente());
-				int aux = cuentaNeg.contarCuentasActivas(cuentasActivas);
-				
-				if(estado) {
-										
-					if(aux >= 3) {
-						mensaje = "El cliente ya posee tres cuentas activas";
-						claseMensaje = "alert alert-danger";
-						request.setAttribute("txtMensajeCuenta", mensaje);
-						request.setAttribute("claseMensajeCuenta", claseMensaje);
-
-						request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
-						return;
-					}
-					
-				}
-				boolean estadoActualizado = cuentaNeg.actualizarEstado(idCuenta, estado);
-				
-				if (estadoActualizado) {
-				
-					if (!estado) {
-						
-						mensaje = "Cuenta desactivada";
-						claseMensaje = "alert alert-success";
-						request.setAttribute("txtMensajeCuenta", mensaje);
-						request.setAttribute("claseMensajeCuenta", claseMensaje);
-
-					} else {
-						mensaje = "Cuenta reactivada";
-						claseMensaje = "alert alert-success";
-						request.setAttribute("txtMensajeCuenta", mensaje);
-						request.setAttribute("claseMensajeCuenta", claseMensaje);
-
-					}
-					
-				}else {
-					request.getRequestDispatcher("ServletCuenta?opcion=buscarCliente&dniCliente=" + request.getParameter("dniCliente")).forward(request, response);
-				}
-				
-				request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
-				
-
-			} catch (NullPointerException e) {
-				mensaje = "Ocurrió un error en el cambio de estado" + e.getMessage();
-				claseMensaje = "alert alert-danger";				
-				request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
-
-			} catch (SQLException e) {
-			
-				e.printStackTrace();
-			}
+			eventoCambiarEstado( request, response) ;
 
 			break;
 
@@ -225,4 +104,139 @@ public class ServletCuenta extends HttpServlet {
 			break;
 		}
 	}
+	
+	public void eventoAgregarCuenta(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		Cuenta cuentagregada = new Cuenta();
+		
+		try {
+
+			Cliente cliente = new Cliente();
+			cliente.setIdCliente(Integer.parseInt(request.getParameter("clienteId")));
+
+			TipoCuenta tipodecuenta = new TipoCuenta();
+			tipodecuenta.setId(Integer.parseInt(request.getParameter("ddlTipoCuenta")));
+			
+			
+
+			cuentagregada.setCliente(cliente);
+			cuentagregada.setFechaCreacion(LocalDateTime.now());
+			cuentagregada.setTipoCuenta(tipodecuenta);
+			cuentagregada.setCbu(request.getParameter("txtCbu"));
+			cuentagregada.setSaldo(new BigDecimal(request.getParameter("txtSaldo")));
+			cuentagregada.setEstado(true);
+
+			boolean cuentaCreada = cuentaNeg.insert(cuentagregada);
+
+			if (cuentaCreada) {
+				mensaje = "La cuenta fue creada con ï¿½xito";
+				claseMensaje = "alert alert-success";
+			} else {
+				mensaje = "La cuenta no pudo ser creada ";
+				claseMensaje = "alert alert-danger";
+			}
+
+		} catch (NullPointerException e) {
+			mensaje = "La cuenta no pudo ser creada " + e.getMessage();
+			claseMensaje = "alert alert-danger";
+			e.printStackTrace();
+
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} 
+		request.setAttribute("txtMensajeCuenta", mensaje);
+		request.setAttribute("claseMensajeCuenta", claseMensaje);
+		request.getRequestDispatcher("/Cuentas.jsp").forward(request, response);
+
+	}
+	public void eventoBuscarCliente(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String dni = request.getParameter("dniCliente");
+
+		try {
+			Cliente clienteServlet = clienteNeg.buscarClienteXDNI(dni);
+
+			if (clienteServlet != null) {
+
+				List<Cuenta> cuentas = cuentaNeg.cuentasXCliente(clienteServlet.getIdCliente());
+
+				request.setAttribute("clienteServlet", clienteServlet);
+				request.setAttribute("cuentasxCliente", cuentas);
+				
+			} else {
+
+				request.setAttribute("errorBusqueda", true);
+			}
+
+			request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
+
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	public void eventoCambiarEstado(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		int idCuenta = Integer.parseInt(request.getParameter("idCuenta"));
+		boolean estado = !Boolean.parseBoolean(request.getParameter("estado"));
+		String dni2 = request.getParameter("dniCliente");
+		Cliente clienteServlet = clienteNeg.buscarClienteXDNI(dni2);
+		
+		try {
+			
+			List<Cuenta> cuentasActivas = cuentaNeg.cuentasXCliente(clienteServlet.getIdCliente());
+			int aux = cuentaNeg.contarCuentasActivas(cuentasActivas);
+			
+			if(estado) {
+									
+				if(aux >= 3) {
+					mensaje = "El cliente ya posee tres cuentas activas";
+					claseMensaje = "alert alert-danger";
+					request.setAttribute("txtMensajeCuenta", mensaje);
+					request.setAttribute("claseMensajeCuenta", claseMensaje);
+
+					request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
+					return;
+				}
+				
+			}
+			boolean estadoActualizado = cuentaNeg.actualizarEstado(idCuenta, estado);
+			
+			if (estadoActualizado) {
+			
+				if (!estado) {
+					
+					mensaje = "Cuenta desactivada";
+					claseMensaje = "alert alert-success";
+					request.setAttribute("txtMensajeCuenta", mensaje);
+					request.setAttribute("claseMensajeCuenta", claseMensaje);
+
+				} else {
+					mensaje = "Cuenta reactivada";
+					claseMensaje = "alert alert-success";
+					request.setAttribute("txtMensajeCuenta", mensaje);
+					request.setAttribute("claseMensajeCuenta", claseMensaje);
+
+				}
+				
+			}else {
+				request.getRequestDispatcher("ServletCuenta?opcion=buscarCliente&dniCliente=" + request.getParameter("dniCliente")).forward(request, response);
+			}
+			
+			request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
+			
+
+		} catch (NullPointerException e) {
+			mensaje = "Ocurriï¿½ un error en el cambio de estado" + e.getMessage();
+			claseMensaje = "alert alert-danger";				
+			request.getRequestDispatcher("Cuentas.jsp").forward(request, response);
+
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 }
